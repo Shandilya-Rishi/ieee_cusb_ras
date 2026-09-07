27ec16e9a9e9025ebecffb99e1308a7b2dd09370
Miyoko-san <miyokofilms@gmail.com>
2026-09-07 10:49:00 +0530

    feat: Remove global top bar and add RAS logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,22 +3,12 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <>
-      {/* Top Global Header Bar */}
-      <div className="global-top-bar">
-        <div className="container global-top-bar-container">
-          <a href="https://www.ieee.org" target="_blank" rel="noopener noreferrer">IEEE.org</a>
-          <a href="https://ieeexplore.ieee.org" target="_blank" rel="noopener noreferrer">IEEE Xplore Digital Library</a>
-          <a href="https://standards.ieee.org" target="_blank" rel="noopener noreferrer">IEEE Standards</a>
-          <a href="https://spectrum.ieee.org" target="_blank" rel="noopener noreferrer">IEEE Spectrum</a>
-          <a href="https://www.ieee.org/sitemap" target="_blank" rel="noopener noreferrer">More Sites</a>
-        </div>
-      </div>
-
       {/* Main Floating Navbar Wrapper */}
       <div className="navbar-wrapper">
         <nav className="navbar">
           <div className="nav-container">
             <Link href="/" className="nav-brand">
+              <img src="/ras-logo.png" alt="RAS Logo" style={{ height: '40px', objectFit: 'contain' }} />
               <span style={{color: 'var(--ras-plum)'}}>IEEE RAS</span> CUSB
             </Link>
             
